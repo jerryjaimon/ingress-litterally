e3b4d519a4c8fc51cddff965666ce5527ebcc96b
Jerry George Jaimon <45946840+jerryjaimon@users.noreply.github.com>
2021-02-14 16:34:29 +0400

    Added python code for QR code generation

---
+++ Object detection/main2.py
@@ -0,0 +1,71 @@
+import numpy as np
+import time
+import cv2
+
+labelsPath = ("coco.names")
+LABELS = open(labelsPath).read().strip().split("\n")
+
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+weightsPath = "yolov3.weights"
+configPath = "yolov3.cfg"
+
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+#ln = net.getLayerNames()
+#ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+vs = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+(W, H) = (None, None)
+
+while True:
+    (grabbed, image) = vs.read()
+    if not grabbed:
+        break
+    (H, W) = image.shape[:2]
+    ln = net.getLayerNames()
+    ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+    # show timing information on YOLO
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    for output in layerOutputs:
+        for detection in output:
+
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > 0.6:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.6, 0.3)
+
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+            cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, color, 2)
+    cv2.imshow("Image", image)
+    key=cv2.waitKey(4)
+    if key == ord('q'):
+        break
+
+vs.release()
+cv2.destroyAllWindows()
